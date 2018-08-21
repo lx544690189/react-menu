fb7c3f900a7ea1104f508bc00d713acddc1edbea
李鑫 <lixin@cai-inc.com>
2018-08-21 11:09:31 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
