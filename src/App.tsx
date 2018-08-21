1401b9919ea84dd6c6b45fabc8cdbcebfaa139a8
李鑫 <lixin@cai-inc.com>
2018-08-21 20:18:54 +0800

    [update]测试一下下

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
-import './App.css';
+import './App.less';
+import ScorpioMenu from './component/Menu';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <ScorpioMenu/>
       </div>
     );
   }
---
+++ src/component/Menu.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Menu.less';
+
+class Menu extends React.Component {
+  public render() {
+    return (
+      <div className="scorpio-menu">
+        scorpio-menu
+      </div>
+    );
+  }
+}
+
+export default Menu;
